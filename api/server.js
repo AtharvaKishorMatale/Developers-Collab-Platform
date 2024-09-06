160a6dfc7747bb6a50c2d68462b325fbdb5ff77c
Arnav Panchal <arnavpanchal27@gmail.com>
2024-09-07 01:04:44 +0530

    feat-initial frontend components

--- api/server.js
+++ api/server.js
@@ -1,20 +1,24 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path')
 require('dotenv').config();
 
 const app = express();
 app.use(express.json());
 
 
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('MongoDB connected'))
   .catch((err) => console.error('MongoDB connection error:', err));
 
-app.get('/', (req, res) => {
-  res.send('Hello, world!');
+// app.get('/', (req, res) => {
+//   res.send('Hello, world!');
+// });
+
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
 });
 
 const PORT = process.env.PORT || 5000;
